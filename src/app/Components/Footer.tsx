25508b23b47b04fd97b43ab2f040b92cf0fd24c8
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2024-11-27 03:08:02 -0700

    set margin

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -4,7 +4,7 @@ import FooterTop from './FooterTop'
 
 const Footer = () => {
   return (
-    <div className='w-full h-auto lg:h-screen flex justify-center items-center'>
+    <div className='w-full h-auto my-20 lg:my-0 lg:h-screen flex justify-center items-center'>
       <FooterTop/>
     </div>
   )
